0bd5d94e862839e7060370d8eb80fbb535ed439f
matucasal <matias.casal@yahoo.com.ar>
2022-08-24 10:57:38 -0300

    adding first deploy contract test

--- deploy.js
+++ deploy.js
@@ -1,24 +1,27 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
-const { interface, bytecode } = require('./compile');
-require('dotenv').config();
+require('dotenv').config(); 
 
-const provider = new HDWalletProvider(
+const { abi, evm } = require('./compile');
+ 
+provider = new HDWalletProvider(
   process.env.MNEMONIC,
 	process.env.INFURA_URL,
 );
+ 
 const web3 = new Web3(provider);
-
+ 
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
-
+ 
   console.log('Attempting to deploy from account', accounts[0]);
-
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode })
+ 
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object })
     .send({ gas: '1000000', from: accounts[0] });
-
+ 
   console.log('Contract deployed to', result.options.address);
   provider.engine.stop();
 };
+ 
 deploy();
